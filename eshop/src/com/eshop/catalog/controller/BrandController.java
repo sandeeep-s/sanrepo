00d42118eddce4fe52eb007a949f22b221148a82
ssd1kor <ssd1kor@BMH316969.BMH.APAC.BOSCH.COM>
2013-05-11 12:20:03 +0530

    Catalog Controller Committed

---
+++ eshop/src/com/eshop/catalog/controller/BrandController.java
@@ -0,0 +1,113 @@
+/**
+ * 
+ */
+package com.eshop.catalog.controller;
+
+import java.util.Set;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eshop.catalog.model.Brand;
+import com.eshop.catalog.service.BrandService;
+import com.eshop.common.service.MediaService;
+
+/**
+ * @author ssd1kor
+ * 
+ */
+@Controller
+@RequestMapping("/brand")
+public class BrandController {
+
+	@Inject
+	@Named("brandService")
+	private BrandService brandService;
+
+	@Inject
+	@Named("mediaService")
+	private MediaService mediaService;
+
+	public BrandService getBrandService() {
+		return brandService;
+	}
+
+	public void setBrandService(BrandService brandService) {
+		this.brandService = brandService;
+	}
+
+	public MediaService getMediaService() {
+		return mediaService;
+	}
+
+	public void setMediaService(MediaService mediaService) {
+		this.mediaService = mediaService;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String listBrands(Model model) {
+		Set<Brand> brands = brandService.getAllBrands();
+		model.addAttribute("brands", brands);
+		return "brandList";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String displayAddBrandForm(Model model) {
+		model.addAttribute("brand", new Brand());
+		return "addBrand";
+	}
+
+	@RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
+	public String displayEditBrandForm(@PathVariable Long id, Model model, HttpServletRequest request) {
+		Brand brand = brandService.getBrandById(id);
+		model.addAttribute("brand", brand);
+
+		return "editBrand";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String addBrand(@Valid Brand brand, BindingResult result, Model model, HttpServletRequest request) {
+		if (result.hasErrors()) {
+			return "addBrand";
+		}
+
+		brand = brandService.addBrand(brand);
+		model.addAttribute("brand", brand);
+		return "addBrandSuccess";
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public String getBrand(@PathVariable Long id, Model model) {
+		Brand brand = brandService.getBrandById(id);
+		model.addAttribute("brand", brand);
+		return "viewBrand";
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public String updateBrand(@Valid Brand brand, BindingResult result, Model model, HttpServletRequest request) {
+
+		if (result.hasErrors()) {
+			return "editBrand";
+		}
+
+		brand = brandService.updateBrand(brand);
+		model.addAttribute("brand", brand);
+		return "editBrandSuccess";
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public String deleteBrand(@PathVariable Long id) {
+		brandService.deleteBrand(id);
+		return "deleteBrandSuccess";
+	}
+
+}
---
+++ eshop/src/com/eshop/catalog/controller/PatternController.java
@@ -0,0 +1,113 @@
+/**
+ * 
+ */
+package com.eshop.catalog.controller;
+
+import java.util.Set;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eshop.catalog.model.Pattern;
+import com.eshop.catalog.service.PatternService;
+import com.eshop.common.service.MediaService;
+
+/**
+ * @author ssd1kor
+ * 
+ */
+@Controller
+@RequestMapping("/pattern")
+public class PatternController {
+
+	@Inject
+	@Named("patternService")
+	private PatternService patternService;
+
+	@Inject
+	@Named("mediaService")
+	private MediaService mediaService;
+
+	public PatternService getPatternService() {
+		return patternService;
+	}
+
+	public void setPatternService(PatternService patternService) {
+		this.patternService = patternService;
+	}
+
+	public MediaService getMediaService() {
+		return mediaService;
+	}
+
+	public void setMediaService(MediaService mediaService) {
+		this.mediaService = mediaService;
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String listPatterns(Model model) {
+		Set<Pattern> patterns = patternService.getAllPatterns();
+		model.addAttribute("patterns", patterns);
+		return "patternList";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String displayAddPatternForm(Model model) {
+		model.addAttribute("pattern", new Pattern());
+		return "addPattern";
+	}
+
+	@RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
+	public String displayEditPatternForm(@PathVariable Long id, Model model, HttpServletRequest request) {
+		Pattern pattern = patternService.getPatternById(id);
+		model.addAttribute("pattern", pattern);
+
+		return "editPattern";
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String addPattern(@Valid Pattern pattern, BindingResult result, Model model, HttpServletRequest request) {
+		if (result.hasErrors()) {
+			return "addPattern";
+		}
+
+		pattern = patternService.addPattern(pattern);
+		model.addAttribute("pattern", pattern);
+		return "addPatternSuccess";
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public String getPattern(@PathVariable Long id, Model model) {
+		Pattern pattern = patternService.getPatternById(id);
+		model.addAttribute("pattern", pattern);
+		return "viewPattern";
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public String updatePattern(@Valid Pattern pattern, BindingResult result, Model model, HttpServletRequest request) {
+
+		if (result.hasErrors()) {
+			return "editPattern";
+		}
+
+		pattern = patternService.updatePattern(pattern);
+		model.addAttribute("pattern", pattern);
+		return "editPatternSuccess";
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public String deletePattern(@PathVariable Long id) {
+		patternService.deletePattern(id);
+		return "deletePatternSuccess";
+	}
+
+}
